7d198e41de998d701483452a1dca138ccff98414
yashilbusa <yashilpatel1914@gmail.com>
2025-02-05 16:08:21 +0530

    commit successsfully

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'about', component: AboutComponent },
-    { path: 'contact', component: ContactComponent }
+    { path: '', title:'Home', component: HomeComponent },
+    { path: 'about',title:'About', component: AboutComponent },
+    { path: 'contact', title:'Contact', component: ContactComponent },
+    { path: '**', title:'Page Not Found', component: PageNotFoundComponent }
 ];
