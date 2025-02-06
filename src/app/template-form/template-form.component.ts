5f002aa6985e6e635a8eb6d0313548885e86cb43
yashilbusa <yashilpatel1914@gmail.com>
2025-02-06 11:39:34 +0530

    commit

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -1,17 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template-form',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './template-form.component.html',
   styleUrl: './template-form.component.css'
 })
 export class TemplateFormComponent {
 
   save(form:any){
-    console.log(form.value);
+    if(form.valid){
+      console.log(form.value);
+    }
     form.reset();
   }
 
