931daa6a717f87b16731ce4f400a156f41e6d8b2
yashilbusa <yashilpatel1914@gmail.com>
2025-02-06 13:57:43 +0530

    commit

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
@@ -8,6 +8,19 @@ import { ReactiveFormsModule } from '@angular/forms';
   templateUrl: './reactive-form.component.html',
   styleUrl: './reactive-form.component.css'
 })
+
 export class ReactiveFormComponent {
 
+  regForm:FormGroup | any;
+  constructor() { }
+
+  ngOnDestroy() {
+    this.regForm = new FormGroup({
+      id:new FormControl(),
+      fullname:new FormControl(),
+      emailadd:new FormControl(),
+      phoneno:new FormControl()
+    })
+    
+  }
 }
