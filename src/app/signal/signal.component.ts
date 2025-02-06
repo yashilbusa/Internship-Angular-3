9db4d4612be5d185e413a48648dbe06a8fe704fd
yashilbusa <yashilpatel1914@gmail.com>
2025-02-06 17:28:52 +0530

    create signal component

---
+++ src/app/signal/signal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signal',
+  standalone: true,
+  imports: [],
+  templateUrl: './signal.component.html',
+  styleUrl: './signal.component.css'
+})
+export class SignalComponent {
+
+}
