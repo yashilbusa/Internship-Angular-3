06045c36ed8556c0c7f312ecd311f6fd3a12778e
yashilbusa <yashilpatel1914@gmail.com>
2025-02-06 17:53:34 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { TemplateFormComponent } from "./template-form/template-form.component";
 import { ReactiveFormComponent } from "./reactive-form/reactive-form.component";
+import { SignalComponent } from "./signal/signal.component";
 // import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterLink, RouterOutlet, TemplateFormComponent, ReactiveFormComponent],
+  imports: [RouterLink, RouterOutlet, TemplateFormComponent, ReactiveFormComponent, SignalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/signal/signal.component.ts
+++ src/app/signal/signal.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-signal',
   standalone: true,
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './signal.component.html',
   styleUrl: './signal.component.css'
 })
-export class SignalComponent {
 
+export class SignalComponent {
+  
+  counter = signal(0);
+
+  username = signal('yashilbusa');
+
+  fruits = signal(["Apple","Pineple","Mango"])
+
+  employeeData = signal({
+    companyName:"InnovateMR",
+    role:"Software Engineer Intern"
+  });
+
+  increase(){
+    this.counter.update(c => c + 1)
+  }
+
+  reset(){
+    this.counter.set(0)
+  }
+
+  constructor() {
+    effect(()=>{
+      console.log(`Value of Counter is ${this.counter()}`);
+    })
+  }
 }
