60f2d52d4da779ab6f7e8d7711cee0c9a2cc9f74
yashilbusa <yashilpatel1914@gmail.com>
2025-02-07 10:19:39 +0530

    put into navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,13 +5,21 @@ import { ContactComponent } from './contact/contact.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AccountComponent } from './account/account.component';
 import { AccountDetailComponent } from './account/account-detail/account-detail.component';
+import { TemplateFormComponent } from './template-form/template-form.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
+import { SignalComponent } from './signal/signal.component';
 
 export const routes: Routes = [
     { path: '', title:'Home', component: HomeComponent },
     { path: 'about',title:'About', component: AboutComponent },
     { path: 'contact', title:'Contact', component: ContactComponent },
     { path: 'account', title:'Account', loadComponent: () => import('./account/account.component').then((c) => c.AccountComponent)},
+    { path: 'tdf', title:'Template Driven Form', component: TemplateFormComponent },
+    { path: 'rf', title:'Reactive Form', component: ReactiveFormComponent },
+    { path: 'signal', component: SignalComponent },
     { path: 'account/:id', component: AccountDetailComponent },
+
     // { path: '', redirectTo:'home', pathMatch:'full'},
     { path: '**', title:'Page Not Found', component: PageNotFoundComponent }
+
 ];
