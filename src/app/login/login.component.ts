484ae2390b3fd9eb4e17e88e101d79ead4b3683b
yashilbusa <yashilpatel1914@gmail.com>
2025-02-07 12:45:43 +0530

    commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,ReactiveFormsModule,RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+    loginForm!: FormGroup;
+    constructor(private fb:FormBuilder) { }
+  
+    ngOnInit() {
+      this.loginForm = this.fb.group({
+        Email:['',[Validators.required, Validators.email]],
+        PassWord:['',[Validators.required, Validators.minLength(8)]]
+      });
+    }
+
+    login(){
+      console.log(this.loginForm.value);
+      this.loginForm.reset();
+    }
+  
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,12 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,ReactiveFormsModule,RouterLink],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+  signupForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.signupForm = this.fb.group({
+      Email: ['', [Validators.required, Validators.email]],
+      PassWord: ['', [Validators.required, Validators.minLength(8)]],
+      ConfirmPassWord: ['', [Validators.required]]
+    });
+  }
+
+  register(){
+    if (this.signupForm.valid) {
+      console.log('Signup Successful', this.signupForm.value);
+    }
+  }
 }
